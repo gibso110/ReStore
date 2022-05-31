e6ed0aeea4699b2fd9e9abe06ac1cf0b95bf8a67
Jarae Gibson <jaraegibs@gmail.com>
2022-05-30 23:08:57 -0400

    converted all of of the products from list items into cards

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,34 +1,21 @@
-import { Typography } from "@mui/material";
+import { CssBaseline, Typography } from "@mui/material";
+import { Container } from "@mui/system";
 import {useState, useEffect} from "react";
 import Catalog from "../../features/catalog/catalog";
 import { Product } from "../models/products";
+import Header from "./Header";
 
 
 function App() {
-  const [products, setProducts] = useState<Product[]>([]);
-
-  useEffect(() => {
-    fetch("http://localhost:5000/api/products")
-    .then(response => response.json())
-    .then(data => setProducts(data))
-  }, [])
-
-  function addProduct(){
-    setProducts(prevState => [...prevState,
-      {
-        id: prevState.length + 101,
-      name: "product" +(prevState.length +1), 
-      price: (prevState.length *100) + 100,
-      brand: "some brand",
-      description: "some discription",
-      pictureUrl: "http://picsum.photos/200"
-    }])
-  };
+  
  
   return (
     <>
-      <Typography variant='h1'>Re-Store</Typography>
-      <Catalog products={products} addProduct={addProduct}/>
+      <CssBaseline />
+      <Header></Header>
+      <Container>
+      <Catalog/>
+      </Container>
       
     </>
   );
