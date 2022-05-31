c94a94eb13f109be7200fca591742d953ba437b5
Jarae Gibson <jaraegibs@gmail.com>
2022-05-30 23:35:49 -0400

    added dark mode

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,4 +1,5 @@
-import { CssBaseline, Typography } from "@mui/material";
+import { ThemeProvider } from "@emotion/react";
+import { createTheme, CssBaseline, Typography } from "@mui/material";
 import { Container } from "@mui/system";
 import {useState, useEffect} from "react";
 import Catalog from "../../features/catalog/catalog";
@@ -7,17 +8,30 @@ import Header from "./Header";
 
 
 function App() {
-  
+  const [darkMode, setDarkMode] = useState(false);
+  const paletteType = darkMode ? "dark" : "light";
+  const theme = createTheme({
+    palette: {
+      mode: paletteType,
+      background: {
+        default: paletteType === "light" ? "#eaeaea" : "#121212"
+      }
+    }
+  })
+
+  function handleThemeChange(){
+    setDarkMode(!darkMode);
+  }
  
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <CssBaseline />
-      <Header></Header>
+      <Header darkMode={darkMode} handleThemeChange={handleThemeChange}></Header>
       <Container>
       <Catalog/>
       </Container>
       
-    </>
+    </ThemeProvider>
   );
 }
 
